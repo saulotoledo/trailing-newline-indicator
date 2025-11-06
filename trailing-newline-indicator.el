f5adae9c30f3c610059601b1ed38f4a4cb12cdb2
Saulo Soares de Toledo <saulo.soares.de.toledo@tietoevry.com>
2025-11-06 14:37:47 +0100

    chore: add license file and header

--- trailing-newline-indicator.el
+++ trailing-newline-indicator.el
@@ -1,4 +1,17 @@
-;;; trailing-newline-indicator.el --- Display an indicator or potential line number to mark the file's trailing newline -*- lexical-binding: t; -*-
+;;; trailing-newline-indicator.el --- Show an indicator for the trailing newline -*- lexical-binding: t; -*-
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 ;; Author: Saulo S. de Toledo <saulotoledo@gmail.com>
 ;; Version: 0.1.0
